75e6786135557e847ba5561e9d8c200224e57c75
revirtears <astralhsssss@gmail.com>
2025-01-24 14:29:11 +0200

    Update api.py

--- site/api.py
+++ site/api.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI, HTTPException, Query, Request
+from fastapi import FastAPI, Request
 import uvicorn
+
+from signature import BotSettings
 
 
 app = FastAPI()
+bot_settings = BotSettings()
 
 
 @app.get("/postback")
 async def handle_postback(request: Request):
-    query_params = request.query_params
-    print(query_params)
-    query_dict = dict(query_params)
-    return {"message": "Query parameters received", "data": query_dict}
+    sub1 = request.query_params.get("sub1") 
 
+    if sub1:
+        print(f"sub1: {sub1}")
+        await bot_settings.db.add_user_ref(uid=sub1)
+
+    return {"message": "Query parameters received", "sub1": sub1}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
